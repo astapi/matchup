9ce31e6c7c76fa9c18eaa262a3f44f0e7bb368ae
astapi <jyunichi.k@gmail.com>
2016-09-11 19:11:07 +0900

    fix syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :matchup, Matchup.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "fathomless-castle-69377", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
